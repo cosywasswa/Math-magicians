df44d4ebbf0741a139d073fb84883f01c02e0029
Cosmas Wasswa <cosywas@gmail.com>
2023-07-12 14:50:35 +0300

    Fix linter errors

--- src/_tests_/navbar.test.js
+++ src/_tests_/navbar.test.js
@@ -4,32 +4,32 @@ import renderer from 'react-test-renderer';
 import Navbar from '../components/navbar';
 
 describe('Nav bar component', () => {
-    it('should render the navbar', () => {
-        const { getByTestId } = render(
-            <BrowserRouter>
-                <Navbar />
-            </BrowserRouter>,
-        );
-        const bar = getByTestId('navi');
-        expect(bar).toBeInTheDocument;
-    });
+  it('should render the navbar', () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    );
+    const bar = getByTestId('navi');
+    expect(bar).toBeInTheDocument;
+  });
 
-    it('should render the custom link with correct element', () => {
-        render(
-            <BrowserRouter>
-                <Navbar />
-            </BrowserRouter>,
-        );
-        const link = screen.getByTestId('border');
-        expect(link).toBeCalled;
-    });
+  it('should render the custom link with correct element', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    );
+    const link = screen.getByTestId('border');
+    expect(link).toBeCalled;
+  });
 
-    it('should render correctly', () => {
-        const tree = renderer.create(
-            <BrowserRouter>
-                <Navbar />
-            </BrowserRouter>,
-        ).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it('should render correctly', () => {
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
--- src/_tests_/operate.test.js
+++ src/_tests_/operate.test.js
@@ -1,23 +1,23 @@
-import operate from '../logic/operate';
 import Big from 'big.js';
+import operate from '../logic/operate';
 
 describe('operate logic ', () => {
-    it('should add the two numbers correctly', () => {
-        expect(operate(Big(4), Big(7), '+')).toBe('11');
-    });
+  it('should add the two numbers correctly', () => {
+    expect(operate(Big(4), Big(7), '+')).toBe('11');
+  });
 
-    it('should subtract the two numbers correctly', () => {
-        expect(operate(Big(80), Big(10), '-')).toBe('70');
-    });
+  it('should subtract the two numbers correctly', () => {
+    expect(operate(Big(80), Big(10), '-')).toBe('70');
+  });
 
-    it('should multiply the two numbers correctly', () => {
-        expect(operate(Big(20), Big(5), 'x')).toBe('100');
-    });
+  it('should multiply the two numbers correctly', () => {
+    expect(operate(Big(20), Big(5), 'x')).toBe('100');
+  });
 
-    it('should devide the two numbers correctly', () => {
-        expect(operate(Big(10), Big(2), '÷')).toBe('5');
-    });
-    it('should throw an error if divided by 0', () => {
-        expect(operate(Big(10), Big(0), '÷')).toBe("Can't divide by 0.");
-    });
+  it('should devide the two numbers correctly', () => {
+    expect(operate(Big(10), Big(2), '÷')).toBe('5');
+  });
+  it('should throw an error if divided by 0', () => {
+    expect(operate(Big(10), Big(0), '÷')).toBe("Can't divide by 0.");
+  });
 });
